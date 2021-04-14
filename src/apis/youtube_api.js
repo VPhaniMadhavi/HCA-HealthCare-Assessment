c9116921764ab8649e17fe69d9d35378bf834472
VPhaniMadhavi <vunguturi.madhavi@gmail.com>
2021-04-14 02:23:28 -0700

    removed api key details

--- src/apis/youtube_api.js
+++ src/apis/youtube_api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyDNnLDKghvwknqExQJbMjUL87LlGvPTl2M';
+const KEY = 'please_input_your_api_developer_key_here';
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
